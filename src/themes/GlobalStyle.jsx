2f9beb0ce173993db2246a46b32adcade3e77ef6
andersonaguiar <54728366+andersonaguia@users.noreply.github.com>
2022-10-01 07:39:00 -0300

    [M1S11] Ex 2 - Configuração do tema utilizando styled components

--- src/themes/GlobalStyle.jsx
+++ src/themes/GlobalStyle.jsx
@@ -1,14 +1,23 @@
-import { createGlobalStyle } from "styled-components"
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    :root {
-        font-size: 62.5%;
-    }
-    body {
-        box-sizing: border-box;
-    }
-    *,*::after, *::before {
-        margin: 0;
-        padding: 0;
-    }
-`
+* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+:root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 400;
+  color-scheme: light dark;
+  color: ${({ theme }) => theme.colors.primary};
+  background-color: ${({ theme }) => theme.colors.backgroundPrimary};
+  font-synthesis: none;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-text-size-adjust: 100%;
+}
+`;
